028d24402cd971e3bf7a10e8d0cf021ac886f76c
swp_song <396587868@qq.com>
2017-10-19 16:39:50 +0800

    3.4.0
    > * 版本版本：3.4.0
    > * 更新时间：2017-10-19 16:38:27
    > * 更新内容：
        * 新增获取设备类型方法

--- SwpToolsDemo/SwpToolsDemo/ViewController.m
+++ SwpToolsDemo/SwpToolsDemo/ViewController.m
@@ -47,6 +47,8 @@
     SwpLog(@"%@", @{@"key" : @"value", @"key1" : @"value1", @"key2" : @"value2"});
     NSLog(@"%@", @{@"key" : @"value", @"key1" : @"value1", @"key2" : @"value2"});
     
+    NSLog(@"%ld", [SwpGetSystemInformation swpGetIphoneDeviceByScreenSize]);
+    
 }
 
 
