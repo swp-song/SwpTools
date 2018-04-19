0fa92adfce9c742fffedca9b19a27df8c45f078a
swp_song <396587868@qq.com>
2018-04-19 23:10:56 +0800

    3.4.5
    > * 版本版本：3.4.5
    > * 更新时间：2018-04-19 23:10:14
    > * 更新内容：
    >>  *  修改打印宏格式

--- SwpTools.podspec
+++ SwpTools.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'SwpTools'
-  s.version          = '3.4.4'
+  s.version          = '3.4.5'
   s.summary          = ' 一些常用的工具方法 '
 
 # This description is used to generate tags and improve search results.
--- SwpTools/SwpTools/SwpLogTools.h
+++ SwpTools/SwpTools/SwpLogTools.h
@@ -10,7 +10,7 @@
 #define SwpLogTools_h
 
 
-#define SwpLogContentInof(format, ...) fprintf(stderr,"\r[PATH   : %s]\r[FILE   : %s, LINE : %d]\r[FUNC   : %s]\r[CONTENT:\r\r   %s\r\r]\r---------------------------------------------------------------------------------------------------------------------", __FILE__, [[[NSString stringWithUTF8String:__FILE__] lastPathComponent] UTF8String], __LINE__, __FUNCTION__, [[NSString stringWithFormat:format, ##__VA_ARGS__] UTF8String]);
+#define SwpLogContentInof(format, ...) fprintf(stderr,"\r---------------------------------------------------------------------------------------------------------------------\r[PATH   : %s]\r[FILE   : %s, LINE : %d]\r[FUNC   : %s]\r[CONTENT:\r\r   %s\r\r]\r---------------------------------------------------------------------------------------------------------------------\r", __FILE__, [[[NSString stringWithUTF8String:__FILE__] lastPathComponent] UTF8String], __LINE__, __FUNCTION__, [[NSString stringWithFormat:format, ##__VA_ARGS__] UTF8String]);
 
 
 #define SwpLog(...) SwpLogContentInof(__VA_ARGS__)
